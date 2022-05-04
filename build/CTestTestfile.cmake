8f42247328c23ac6e1947bd049870195b3ad0778
odj3460 <odj3460@gmail.com>
2022-05-04 09:53:56 +0900

    testest

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/odj/git_teset/src
+# Build directory: /home/odj/git_teset/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
